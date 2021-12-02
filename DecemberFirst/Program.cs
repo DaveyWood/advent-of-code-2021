484195e766a215324b18de7acd14bb9668504cd6
Davey Wood <dwood@cvent.com>
2021-12-01 20:15:39 -0500

    December 1st

---
+++ DecemberFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var numbers = File.ReadAllLines("./input").Select(num => int.Parse(num)).ToArray();
+
+var answer = 0;
+
+Console.WriteLine(numbers.Length);
+
+for(var i = 3; i < numbers.Length; i++)
+{
+  if (numbers[i] > numbers[i-3])
+  {
+    answer++;
+  }
+}
+
+Console.WriteLine(answer);
