8e1cff93202a302940ddc1ff6885c5974cf71af6
Davey Wood <dwood@cvent.com>
2021-12-04 21:45:29 -0500

    add loop to get post-jit performance

--- DecemberFourth/Program.cs
+++ DecemberFourth/Program.cs
@@ -1,68 +1,111 @@
 ﻿var watch = new System.Diagnostics.Stopwatch();
-watch.Start();
-var data = File.ReadAllLines("input");
-Console.WriteLine($"File read at {watch.ElapsedMilliseconds}ms");
 
-var numbersToCall = data[0].Split(',').Select(int.Parse).ToArray();
+// run in loop to get times
+var results = new List<dynamic>();
+for (var runs = 0; runs < 20; runs++)
+{  
+  watch.Reset();
+  watch.Start();
+  var data = File.ReadLines("input").GetEnumerator();
 
-var boards = new List<BingoBoard>();
+  var readFileTime = watch.Elapsed.TotalMilliseconds;
 
-for (var i = 1; i < data.Length; i += 6)
-{
-  var boardNumbers = new Space[5, 5];
-  // i is a blank line
-  // the next five lines are the board
-  for (var j = 0; j < 5; j++)
+  data.MoveNext();
+  var numbersToCall = ParsedNumbers(data.Current);
+
+  IEnumerable<int> ParsedNumbers(string commaSeperated)
   {
-    var lineNumbers = data[i + j + 1].Split(' ').Where(s => s.Length > 0).Select(int.Parse).ToArray();
-    for (var k = 0; k < 5; k++)
+    int buffer = 0;
+    for (int i = 0; i < commaSeperated.Length; i++)
     {
-      boardNumbers[j, k] = new Space { called = false, number = lineNumbers[k] };
+      if (commaSeperated[i] == ',')
+      {
+        yield return buffer;
+        buffer = 0;
+      }
+      else
+      { 
+        buffer = buffer * 10 + commaSeperated[i] - '0';
+      }
     }
   }
 
-  boards.Add(new BingoBoard(boardNumbers));
-}
-Console.WriteLine($"Boards built at {watch.ElapsedMilliseconds}");
+  var boards = new List<BingoBoard>();
+  var listCreatedTime = watch.Elapsed.TotalMilliseconds;
 
-var winningScore = 0;
-var lastBoardScore = 0;
-
-var copyOfBoards = new List<BingoBoard>();
-copyOfBoards.AddRange(boards);
-foreach (var number in numbersToCall)
-{
-  var boardsToRemove = new List<BingoBoard>();
-  foreach (var board in copyOfBoards)
+  while(data.MoveNext())
   {
-    if (board.CallNumber(number))
+    if (data.Current == "")
     {
-      if (winningScore == 0)
+      continue;
+    }
+    var boardNumbers = new Space[5, 5];
+    // the next five lines are the board
+    for (var j = 0; j < 5; j++)
+    {
+      var lineNumbers = data.Current.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+      for (var k = 0; k < 5; k++)
       {
-        winningScore = board.SumOfUnmarked * number;
-        Console.WriteLine("winningScore: " + winningScore);
+        boardNumbers[j, k] = new Space { called = false, number = int.Parse(lineNumbers[k]) };
       }
-      if (copyOfBoards.Count == 1)
+      data.MoveNext();
+    }
+    boards.Add(new BingoBoard(boardNumbers));
+  }
+
+  var boardsBuiltTime = watch.Elapsed.TotalMilliseconds;
+
+  var winningScore = 0;
+  var lastBoardScore = 0;
+
+  var copyOfBoards = new List<BingoBoard>();
+  copyOfBoards.AddRange(boards);
+  foreach (var number in numbersToCall)
+  {
+    var boardsToRemove = new List<BingoBoard>();
+    foreach (var board in copyOfBoards)
+    {
+      if (board.CallNumber(number))
       {
-        lastBoardScore = board.SumOfUnmarked * number;
-        Console.WriteLine("lastBoardScore: " + lastBoardScore);
+        if (winningScore == 0)
+        {
+          winningScore = board.SumOfUnmarked * number;
+        }
+        if (copyOfBoards.Count == 1)
+        {
+          lastBoardScore = board.SumOfUnmarked * number;
+        }
+        boardsToRemove.Add(board);
       }
-      boardsToRemove.Add(board);
-      // Console.WriteLine("some board score: " + board.SumOfUnmarked * number);
-      // board.Print();
+    }
+    boardsToRemove.ForEach(b => copyOfBoards.Remove(b));
+    if (copyOfBoards.Count == 0)
+    {
+      break;
     }
   }
-  boardsToRemove.ForEach(b => copyOfBoards.Remove(b));
-  if (copyOfBoards.Count == 0)
+  var doneTime = watch.Elapsed.TotalMilliseconds;
+  watch.Stop();
+
+  if (runs > 1)
   {
-    break;
+    results.Add(new { winningScore, lastBoardScore, readFileTime, listCreatedTime, boardsBuiltTime, doneTime });
   }
 }
 
-// boards.ForEach(b => b.Print());
+// print results
+var aggregates = new { results[0].winningScore, results[0].lastBoardScore, 
+  readFileTime = new { min = results.Select(r => r.readFileTime).Min(),
+                       max = results.Select(r => r.readFileTime).Max() }, 
+  listCreatedTime = new { min = results.Select(r => r.listCreatedTime).Min(),
+                       max = results.Select(r => r.listCreatedTime).Max() }, 
+  boardsBuiltTime = new { min = results.Select(r => r.boardsBuiltTime).Min(),
+                       max = results.Select(r => r.boardsBuiltTime).Max() }, 
+  doneTime = new { min = results.Select(r => r.doneTime).Min(),
+                       max = results.Select(r => r.doneTime).Max() }
+ };
 
-Console.WriteLine($"Done at {watch.ElapsedMilliseconds}");
-
+ Console.WriteLine(aggregates);
 public struct Space
 {
   public bool called;
@@ -70,7 +113,7 @@ public struct Space
 
   public override string ToString()
   {
-    return number + " " + called + " ";
+    return number + " " + (called ? "True  " : "False ");
   }
 }
 
@@ -118,7 +161,7 @@ public struct BingoBoard
     {
       throw new Exception();
     }
-    // Console.WriteLine("found number " + space.number);
+
     space.called = true;
     _spaces[index.Item1, index.Item2] = space;
 
@@ -141,7 +184,7 @@ public struct BingoBoard
           }
         }
       }
-      // Console.WriteLine("sum: " + sum);
+
       return sum;
     }
   }
