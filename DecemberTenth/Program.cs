d9f287eb47d1da9775bea01b502a07d87f22ed31
Davey Wood <dwood@cvent.com>
2021-12-10 19:08:43 -0500

    part 1 answer for December 10th

---
+++ DecemberTenth/Program.cs
@@ -0,0 +1,65 @@
+﻿var stopwatch = new System.Diagnostics.Stopwatch();
+
+for (var run = 0; run < 5; run++)
+{
+  stopwatch.Restart();
+
+  var input = File.ReadLines("input");
+
+  var openers = new HashSet<char>{ '(', '{', '[', '<' };
+  var closers = new HashSet<char>{ ')', '}', ']', '>' };
+
+  var scores = new Dictionary<char, int> {
+    { ')', 3 },
+    { ']', 57 },
+    { '}', 1197 },
+    { '>', 25137 }
+  };
+
+  bool Matches(char opener, char closer)
+  {
+    return (opener == '(' && closer == ')')
+      || (opener == '[' && closer == ']')
+      || (opener == '{' && closer == '}')
+      || (opener == '<' && closer == '>');
+  }
+
+  char CheckLine(string line)
+  {
+    if (openers == null || closers == null)
+    {
+      throw new Exception("What the fuck?");
+    }
+    var stuff = new Stack<char>();
+    foreach(var character in line)
+    {
+      if (openers.Contains(character))
+      {
+        stuff.Push(character);
+      }
+      else if (closers.Contains(character))
+      {
+        var opener = stuff.Pop();
+        if (!Matches(opener, character))
+        {
+          return character;
+        }
+      }
+    }
+    return 'x';
+  }
+  // fastest time in series was 0.5655 milliseconds
+  // var score = input.Select(CheckLine).Where(c => c != 'x')
+  //   .Select(c => scores[c]).Sum();
+
+  // fastest async time was 0.2285 milliseconds
+  var tasks = input.Select(c => Task.Run<char>(() => CheckLine(c))).ToArray();
+  Task.WaitAll(tasks);
+
+  var score = tasks.Select(t => t.Result).Where(c => c != 'x')
+    .Select(c => scores[c]).Sum();
+
+  var time = stopwatch.Elapsed.TotalMilliseconds;
+
+  Console.WriteLine(new { score, time });
+}
